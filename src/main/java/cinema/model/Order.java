95a56f24e63e2e26de68f4c87557321386363ea7
Mykola Voronov <www.kolyavoronov@gmail.com>
2022-01-19 15:10:41 +0200

    spring part was added to the project

--- src/main/java/cinema/model/Order.java
+++ src/main/java/cinema/model/Order.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,12 +20,13 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
-    @JoinTable(joinColumns = @JoinColumn(name = "order_id"),
+    @JoinTable(name = "orders_tickets",
+            joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
-    @Column(name = "order_date")
-    private LocalDateTime orderDate;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "order_time")
+    private LocalDateTime orderTime;
+    @ManyToOne
     private User user;
 
     public Long getId() {
@@ -45,12 +45,12 @@ public class Order {
         this.tickets = tickets;
     }
 
-    public LocalDateTime getOrderDate() {
-        return orderDate;
+    public LocalDateTime getOrderTime() {
+        return orderTime;
     }
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
     }
 
     public User getUser() {
@@ -66,7 +66,7 @@ public class Order {
         return "Order{"
                 + "id=" + id
                 + ", tickets=" + tickets
-                + ", orderDate=" + orderDate
+                + ", orderTime=" + orderTime
                 + ", user=" + user + '}';
     }
 }
